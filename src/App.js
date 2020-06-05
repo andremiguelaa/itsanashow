aba3c9a7c3c91f1132b86439a9362f34f95dc313
André Ascensão <andremiguelaa@gmail.com>
2020-06-05 22:41:30 +0100

    bootstrap changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import classes from './App.module.scss';
+
+const App = () => (
+  <div className={classes.app}>
+    <img src={logo} className={classes.appLogo} alt="logo" />
+  </div>
+);
 
 export default App;
