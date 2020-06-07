4e36a873889d05c4e1f7f0c64f45232030f5010d
André Ascensão <andremiguelaa@gmail.com>
2020-06-07 20:08:23 +0100

    add coming soon

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useScrollPosition } from '@n8tb1t/use-scroll-position';
+import anime from 'animejs';
 import video from 'assets/video.mp4';
 import frame from 'assets/frame.png';
 import classes from './Video.module.scss';
+
+const startAnimation = (selector) =>
+  anime({
+    targets: selector,
+    opacity: 1,
+    scale: 1,
+    easing: 'easeInBounce',
+    delay: (_, index) => index * 80,
+    direction: 'alternate',
+    loop: true,
+  });
 
 const Video = () => {
   const [videoScroll, setVideoScroll] = useState(0);
@@ -12,9 +24,24 @@ const Video = () => {
     null,
     true
   );
+  useEffect(() => {
+    startAnimation(`.${classes.letter}`);
+  }, []);
+
   return (
     <section className={classes.video} style={{ top: `-${videoScroll}px` }}>
       <video autoPlay loop muted src={video} poster={frame}></video>
+      <div className={classes.soon}>
+        {'Coming soon'.split('').map((letter, index) => (
+          <div key={`${letter}-${index}`} className={classes.letter}>
+            {letter !== ' ' ? letter : <>&nbsp;</>}
+          </div>
+        ))}
+      </div>
+      <link
+        href="https://fonts.googleapis.com/css?family=Unica+One"
+        rel="stylesheet"
+      ></link>
       <div
         className={`${classes.scrollContainer} ${
           videoScroll ? classes.hidden : ''
