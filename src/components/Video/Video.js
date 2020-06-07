bba968ca35fc7180b9fe019630fd226ac98bf6fd
André Ascensão <andremiguelaa@gmail.com>
2020-06-07 15:05:11 +0100

    video scroll

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import video from 'assets/video.mp4';
 import classes from './Video.module.scss';
 
-const Video = () => (
-  <section className={classes.video}>
-    <video autoPlay loop muted src={video}></video>
-  </section>
-);
+const Video = () => {
+  const [videoScroll, setVideoScroll] = useState(0);
+  useScrollPosition(
+    ({ currPos }) => setVideoScroll(currPos.y / 4),
+    [],
+    null,
+    true
+  );
+  return (
+    <section className={classes.video} style={{ top: `-${videoScroll}px` }}>
+      <video autoPlay loop muted src={video}></video>
+    </section>
+  );
+};
 export default Video;
