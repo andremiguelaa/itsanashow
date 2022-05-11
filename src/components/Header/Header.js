bee6dc0ac269ddc3d537e57ef248e45876756d48
André Ascensão <andremiguelaa@gmail.com>
2022-05-11 22:09:24 +0100

    small improvements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,6 +24,7 @@ const Header = ({ setModal }) => {
               <img className={classes.logo} src={logo} alt="logo" />
             </Link>
             <nav>
+              <Link to="/work">Our work</Link>
               <Link to="/us">Know us</Link>
               <button
                 onClick={() => {
