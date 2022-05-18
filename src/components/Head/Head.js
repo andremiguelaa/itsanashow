4954f574148aec89ab953d413d5232b1da2452ae
André Ascensão <andremiguelaa@gmail.com>
2022-05-18 01:48:31 +0100

    head animation improvement

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,46 +1,54 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Lottie from 'react-lottie-player';
+import { useInView } from 'react-intersection-observer';
 
 import head from 'assets/head.json';
 
 const headFrameLimits = {
-  start: 44,
+  start: 0,
+  // middle1: 32,
+  middle: 44,
   end: 61,
 };
 
 const Head = ({ className }) => {
-  const headFrame = useRef(headFrameLimits.start);
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const headFrame = useRef(headFrameLimits.middle);
   const headTimer = useRef();
   const [headFrameState, setHeadFrameState] = useState(headFrameLimits.start);
 
-  const numberChange = (direction) => {
+  const numberChange = (start, end) => {
+    headFrame.current = start;
     clearInterval(headTimer.current);
     headTimer.current = setInterval(() => {
-      if (direction === '+') {
-        if (headFrame.current === headFrameLimits.end) {
-          clearInterval(headTimer.current);
-        } else {
-          headFrame.current = headFrame.current + 1;
-        }
+      const direction = start > end ? -1 : 1;
+      if (headFrame.current === end) {
+        clearInterval(headTimer.current);
       } else {
-        if (headFrame.current === headFrameLimits.start) {
-          clearInterval(headTimer.current);
-        } else {
-          headFrame.current = headFrame.current - 1;
-        }
+        headFrame.current = headFrame.current + direction;
       }
       setHeadFrameState(headFrame.current);
     }, 40);
   };
 
+  useEffect(() => {
+    if (inView) {
+      numberChange(headFrameLimits.start, headFrameLimits.middle);
+    } else {
+      headFrame.current = headFrameLimits.start;
+      setHeadFrameState(headFrame.current);
+    }
+  }, [inView]);
+
   return (
     <div
+      ref={ref}
       className={className}
       onMouseEnter={() => {
-        numberChange('+');
+        numberChange(headFrameLimits.middle, headFrameLimits.end);
       }}
       onMouseLeave={() => {
-        numberChange('-');
+        numberChange(headFrameLimits.start, headFrameLimits.middle);
       }}
     >
       <Lottie animationData={head} goTo={headFrameState} />
