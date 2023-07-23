92212a62d87ca8abe4bece36f28eedd651a0ff60
André Ascensão <andremiguelaa@gmail.com>
2023-07-23 01:42:32 +0100

    instagram section

--- src/components/Instagram/Instagram.js
+++ src/components/Instagram/Instagram.js
@@ -1,19 +1,124 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
+import AppContext from 'AppContext';
 import useRequest from 'utils/useRequest';
+import AnimatedText from 'components/AnimatedText/AnimatedText';
 
 import classes from './Instagram.module.scss';
 
 const Instagram = () => {
+  const { setCursorType } = useContext(AppContext);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    centerMode: false,
+    variableWidth: true,
+    touchThreshold: 100,
+    beforeChange: (_, next) => {
+      setCurrentSlide(next);
+    },
+  };
+
   const { data } = useRequest({
-    url: 'me/media',
+    url: 'instagram',
     method: 'GET',
-    baseURL: 'https://graph.instagram.com/v14.0/',
   });
 
-  console.log(data);
+  const postToShow = data?.slice(0, 10);
 
-  return <>{data && <section className={classes.instagram}></section>}</>;
+  return (
+    <>
+      {data && (
+        <section className={classes.instagram}>
+          <div className="wrapper">
+            <div className={classes.instagramContent}>
+              <header>
+                <div>
+                  <p className={classes.lead}>
+                    <AnimatedText>Follow the fun</AnimatedText>
+                  </p>
+                  <p className={classes.description}>
+                    <AnimatedText delay={250}>
+                      Enter the Insta-Verse
+                    </AnimatedText>
+                  </p>
+                </div>
+                <div className={classes.scrollStatus}>
+                  <div
+                    className={classes.scrollStatusBar}
+                    style={{
+                      width: `${100 / postToShow.length}%`,
+                      transform: `translateX(${currentSlide * 100}%)`,
+                    }}
+                  />
+                </div>
+              </header>
+              <div className={classes.list}>
+                <div
+                  onMouseEnter={() => {
+                    setCursorType('drag');
+                  }}
+                  onMouseLeave={() => {
+                    setCursorType('default');
+                  }}
+                  onMouseDown={() => {
+                    setCursorType('dragging');
+                  }}
+                  onMouseUp={() => {
+                    setCursorType('drag');
+                  }}
+                >
+                  <Slider {...settings}>
+                    {postToShow.map(
+                      ({
+                        id,
+                        caption,
+                        media_url,
+                        media_type,
+                        thumbnail_url,
+                      }) => (
+                        <div key={id}>
+                          <div className={classes.item}>
+                            {media_type === 'VIDEO' && (
+                              <img src={thumbnail_url} alt={caption} />
+                            )}
+                            {media_type !== 'VIDEO' && (
+                              <img src={media_url} alt={caption} />
+                            )}
+                          </div>
+                        </div>
+                      )
+                    )}
+                  </Slider>
+                </div>
+              </div>
+              <div className={classes.linkWrapper}>
+                <a
+                  className={classes.link}
+                  href="https://www.instagram.com/itsanashow.studio/"
+                  rel="noreferrer"
+                  target="_blank"
+                  onMouseEnter={() => {
+                    setCursorType('bigger');
+                  }}
+                  onMouseLeave={() => {
+                    setCursorType('default');
+                  }}
+                >
+                  @itsanashow.studio
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default Instagram;
