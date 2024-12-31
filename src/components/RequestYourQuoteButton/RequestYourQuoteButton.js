6a5d3658bf73452a022399c287996c896fe1309b
André Ascensão <andre.ascensao@bitsighttech.com>
2024-12-31 01:02:57 +0000

    fix request button

--- src/components/RequestYourQuoteButton/RequestYourQuoteButton.js
+++ src/components/RequestYourQuoteButton/RequestYourQuoteButton.js
@@ -1,4 +1,4 @@
-import React, { useRef, useContext } from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
 import Lottie from "react-lottie-player";
 import { useRouter } from "next/navigation";
 
@@ -7,23 +7,58 @@ import CTA01 from "src/assets/buttons/CTA01.json";
 
 import classes from "./RequestYourQuoteButton.module.scss";
 
+const buttonFrameLimits = {
+  startIn: 0,
+  endIn: 32,
+  startOut: 64,
+  endOut: 95,
+};
+
 const RequestYourQuoteButton = () => {
   const router = useRouter();
-  const lottie = useRef();
   const { setCursorType } = useContext(AppContext);
+
+  const buttonFrame = useRef(buttonFrameLimits.startIn);
+  const buttonTimer = useRef();
+  const [buttonFrameState, setButtonFrameState] = useState(
+    buttonFrameLimits.startIn
+  );
+
+  const numberChange = (start, end) => {
+    buttonFrame.current = start;
+    clearInterval(buttonTimer.current);
+    buttonTimer.current = setInterval(() => {
+      const direction = start > end ? -1 : 1;
+      if (buttonFrame.current === end) {
+        clearInterval(buttonTimer.current);
+      } else {
+        buttonFrame.current = buttonFrame.current + direction;
+      }
+      setButtonFrameState(buttonFrame.current);
+    }, 20);
+  };
+
+  useEffect(
+    () => () => {
+      clearInterval(buttonTimer.current);
+    },
+    []
+  );
+
   return (
     <button
       className={classes.button}
       onMouseEnter={() => {
+        numberChange(buttonFrameLimits.startIn, buttonFrameLimits.endIn);
         setCursorType("bigger");
-        lottie.current.goToAndPlay(0);
       }}
       onMouseLeave={() => {
+        numberChange(buttonFrameLimits.startOut, buttonFrameLimits.endOut);
         setCursorType("default");
       }}
       onClick={() => router.push("/contact-us")}
     >
-      <Lottie ref={lottie} animationData={CTA01} loop={false} />
+      <Lottie animationData={CTA01} goTo={buttonFrameState} />
     </button>
   );
 };
