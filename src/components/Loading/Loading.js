9546bda4129bde1a6992fb96ae3ad71a1e019512
André Ascensão <andre.ascensao@bitsighttech.com>
2025-01-25 19:25:42 +0000

    set loading animations fps

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie-player";
 import classNames from "classnames";
 
@@ -6,18 +6,39 @@ import animation from "src/assets/intro-eye.json";
 
 import classes from "./Loading.module.scss";
 
+let interval;
+const ANIMATION_FRAMES = 64;
+const ANIMATION_DURATION = 5000;
+const FPS = 12;
+const step_duration = 1000 / FPS;
+const total_steps = ANIMATION_DURATION / step_duration;
+const frame_interval = ANIMATION_FRAMES / total_steps;
+
 const Loading = () => {
-  const [done, setDone] = useState(false);
+  const [done, setDone] = useState();
+  const [currentFrame, setCurrentFrame] = useState(0);
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      setCurrentFrame((prev) => prev + frame_interval);
+    }, step_duration);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentFrame >= ANIMATION_FRAMES) {
+      clearInterval(interval);
+      setDone(true);
+    }
+  }, [currentFrame]);
+
   return (
     <div className={classNames(classes.loading, { [classes.done]: done })}>
-      <Lottie
-        animationData={animation}
-        play
-        loop={false}
-        onComplete={() => {
-          setDone(true);
-        }}
-      />
+      <div className={classes.loadingWrapper}>
+        <Lottie animationData={animation} goTo={currentFrame} />
+      </div>
     </div>
   );
 };
